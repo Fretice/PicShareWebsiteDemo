ed6da5f73855d87248be4641255aa16afeb49277
Justtery <justteryforme@gmail.com>
2016-12-11 22:58:35 +0800

    insert image layout and pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,29 @@
-from flask import render_template
+from flask import render_template, request, jsonify, current_app
+from flask.ext.login import login_required, current_user
 from . import main
+from ..import db
+from ..models import Pic
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    pics = Pic.query.all()
+    page = request.args.get('page', 1, type=int)
+    pagination = Pic.query.order_by(Pic.upload_time.desc()).paginate(page,
+                    per_page=current_app.config['WEBSITE_PIC_PER_PAGE'],
+                    error_out=False)
+    pics = pagination.items
+    return render_template('index.html', pics=pics, pagination=pagination)
+
+
+@main.route('/save_img', methods=['POST'])
+@login_required
+def save_img():
+    tag = request.form['tag']
+    img_url = request.form['img_url']
+    pic = Pic(pic_url=img_url,
+              uploader_id=current_user.id,
+              tag_name=tag)
+    db.session.add(pic)
+    db.session.commit()
+    return jsonify(tag + ':' + img_url)
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
-    WEBSITE_PIC_PER_PAGE = 20  # how many pics in a page
+    WEBSITE_PIC_PER_PAGE = 10  # how many pics in a page
     WEBSITE_FOLLOWERS_PER_PAGE = 50  # how many followers in a page
     WEBSITE_COMMENTS_PER_PAGE = 30  # how many comments in a page
 
