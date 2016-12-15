b3078a0ef882deb2b83fad969608e3331ebff4b8
Justtery <justteryforme@gmail.com>
2016-12-15 23:22:36 +0800

    add pic_detail comment action

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
-from flask import render_template, request, jsonify, current_app
+from flask import render_template, request, jsonify, current_app, redirect, url_for
 from flask.ext.login import login_required, current_user
 from . import main
 from ..import db
-from ..models import Pic, User
+from ..models import Pic, User, Comment
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -36,11 +36,24 @@ def user(name):
                     per_page=current_app.config['WEBSITE_PIC_PER_PAGE'],
                     error_out=False)
     pics = pagination.items
-    return render_template('user.html', page=page, user=user,\
-                        pagination=pagination,pics=pics)
+    return render_template('user.html', page=page, user=user,
+                           pagination=pagination, pics=pics)
 
 
 @main.route('/pic/<id>', methods=['GET', 'POST'])
 def pic(id):
     pic = Pic.query.filter_by(id=id).first_or_404()
-    return render_template('pic_detail.html', pic=pic)
+    comments = pic.comments
+    return render_template('pic_detail.html', pic=pic, comments=comments)
+
+
+@main.route('/pic/save_comment', methods=['POST'])
+def save_comment():
+    comment_body = request.form['comment']
+    pic_id = request.form['pic_id']
+    comment = Comment(body=comment_body,
+                      author_id=current_user.id,
+                      pic_id=pic_id)
+    db.session.add(comment)
+    db.session.commit()
+    return jsonify(comment_body + ':' + pic_id)
